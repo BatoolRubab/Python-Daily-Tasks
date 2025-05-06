c658521186e81707f5101992d8b7cd84fedd3388
Rubab Batool <113282419+BatoolRubab@users.noreply.github.com>
2025-05-06 18:40:09 +0500

    Add files via upload

---
+++ Day2/Number-Manipulation/task.py
@@ -0,0 +1,8 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+print(int(bmi))
+print(round(bmi , 3))
+score = 10
+height = 5.0
+winnig = True
+print(f"Score is {score}, height is {height} and your wining is {winnig}")
