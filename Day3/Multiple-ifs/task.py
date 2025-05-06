70ba62b7b7fab9cf79ba9090d71c767d7c4dc8cf
Rubab Batool <113282419+BatoolRubab@users.noreply.github.com>
2025-05-06 18:51:08 +0500

    Add files via upload

---
+++ Day3/Multiple-ifs/task.py
@@ -0,0 +1,22 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        bill = 5
+        print("Children tickets are  $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    take_photo = input("Do you want to have a photo take? Type y fpr yes and n for no ")
+    if take_photo == "y":
+        bill += 3
+    print(f"Your final bill is ${bill}")
+else:
+    print("Sorry you have to grow taller before you can ride.")
