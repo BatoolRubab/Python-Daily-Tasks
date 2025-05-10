36d99722cbcce5c4459a9cb552f107d9c75c7d5b
Rubab Batool <113282419+BatoolRubab@users.noreply.github.com>
2025-05-10 17:23:56 +0500

    Add files via upload

---
+++ Day8/Functions/task.py
@@ -0,0 +1,11 @@
+# functions with more than 1 inputs , calling positional arguments
+
+def greet_with(name , location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+# greet_with("Rubab","Islamabad")
+
+# calling with keyword arguments
+greet_with(location="Islamabad",name="Rubab")
+
+
