6e40f2bbb359d814364fb147786921a941e4ced3
Rubab Batool <113282419+BatoolRubab@users.noreply.github.com>
2025-05-12 17:48:03 +0500

    Add files via upload

---
+++ Day9/Function-with-output/task.py
@@ -0,0 +1,18 @@
+
+# def format_name(f_name , l_name):
+#     f1 = (f_name.title())
+#     f2 = (l_name.title())
+#     print(f"{f1} {f2}")
+#
+# format_name(f_name= "RUBab",l_name= "batool")
+
+def function1(text):
+    return text + text
+def function2(text):
+    return text.title()
+
+output = function1('Hello')
+print(output)
+
+output2 = function2("baTOol")
+print(output2)
